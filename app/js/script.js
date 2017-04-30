0343ad3d7019597ed878424f22e7e9f74d405a93
Your Name <you@example.com>
2017-04-30 17:36:14 +0300

    add mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,97 @@
 $(document).ready(function(){
 
+	//validate form
+	$('.main-form form').each(function() {   
+			var curentForm = $(this);
+	    $(this).validate({        
+			    	rules:{ //правила для полей 
+							name:{
+								required:true,
+							},
+							phone:{
+								required:true,
+								minlength:5,
+								number:true
+							},
+							comment:{
+								required:true,
+								minlength:5,
+							},
+						},
+						messages:{
+							name:{
+								required: 'Обязательное поле',
+							},
+							phone:{
+								required: 'Обязательное поле',
+								number:'Введите правильный номер',
+								minlength:'Номер должен быть длиннее',
+							},
+							comment:{
+								required: 'Обязательное поле',
+								minlength:'Сообщение должно быть длиннее',
+							},
+						},
+						submitHandler : function(form){ 
+							$.ajax({ //отправка ajax
+							            type: "POST",
+							            url: "/wp-content/themes/AAK/sender.php",
+							            data: $(form).serialize(),
+							            timeout: 3000,
+							          });
+								$('.modal-close').click(); // автозакрытие окна
+								setTimeout(function(){
+											$('.modal-true').bPopup({
+												closeClass:'modal-close',
+													position:['auto','auto'], // position center
+													follow: [true,true],
+													autoClose: 2000
+											}); 
+											$(':input','.validate-form')
+											  .not(':button, :submit, :reset, :hidden')
+											  .val('')
+											  .removeAttr('checked')
+											  .removeAttr('selected')
+								},2000)
+								
+					}
+			    });
+			});
+		
+
+	//scroll
+     $("a[rel='m_PageScroll2id']").mPageScroll2id({
+		    highlightClass:'menu-el--active',
+		    offset:'100'
+	});
+	
+	//slider
+	$(".cert").owlCarousel({
+	 items : 1,
+		responsive : {
+		 		0:{
+				 	items : 1
+			 	},
+			 	768:{
+				 	items : 2
+			 	},
+			 	960:{
+				 	items : 4
+			 	},
+			 	
+		  },
+	 margin:50,
+	 autoHeight : true,
+	 dots: false,
+	 autoplay : true,
+	 nav:true,
+	 navText:[
+			'<svg class="slider-control"><use xlink:href="#arrow-left"></use></svg>',
+			'<svg class="slider-control"><use xlink:href="#arrow-right"></use></svg>'
+	 ]
+	 }
+	);
+	$(".fancybox").fancybox();
 	/* ###### For only ies  ######*/
 	//if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)){
 	//	//code
@@ -15,47 +107,28 @@ $(document).ready(function(){
 		$('body').empty();
 		$('body').prepend('<div class="old-browser"><div class="old-browser-text"> Браузер не поддерживается =(</div></div>');
 	}
-
-	//for init SVG 
-	svg4everybody();
 	
 	/* ###### For SlideToggle Elements  ######*/
-	/*var hideToggle = function(targetClick,toggleEl) {
+	var hideToggle = function(targetClick,toggleEl) {
 		$(targetClick).click(function(event){
 				event.stopPropagation();
 				$(toggleEl).slideToggle("fast");
-		});
-		$(toggleEl).on("click", function (event) {
-			event.stopPropagation();
 		});
 		$(document).on("click", function () {
 				$(toggleEl).hide();
 		});
 	}
-	hideToggle('.icon-bars','.top-menu_link');*/
+	hideToggle('.nav-toggle','.menu-wrap');
 
-
-	/* ###### init RangeSLider  ######*/
-	/* ###### bower i --save-dev nouislider  ######*/
-	/* ###### https://gist.github.com/fantazer/2bdc4e6a63708e143718ffa7c32eae17  ######*/
-
-	/*var slider = document.getElementById('rangeSlider'); //Элемент
-
-	noUiSlider.create(slider, {
-		start: [0, 100],
-		connect: true,
-		step: 10,
-		range: {
-			'min': 0,
-			'max': 100,
-		},
-		pips: { // Show a scale with the slider
-			mode: 'steps',
-			density: 4
-		}
-	});*/
-
+	//for init SVG 
+	svg4everybody();
+	localStorage.clear();
+	sessionStorage.clear();
 	
+	//clear local storage
+	$(window).unload(function(){
+	  localStorage.clear();
+	});
 })
 
 //cash SVG
